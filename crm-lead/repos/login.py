706f199d65d0bfe9c3cb6b6794d8de584ecf39b4
GunalanD95 <Guna19may2015@gmail.com>
2021-10-11 07:48:19 +0530

    update

--- crm-lead/repos/login.py
+++ crm-lead/repos/login.py
@@ -1,6 +1,61 @@
-from fastapi import APIRouter, Depends # importing api router
-from fastapi import FastAPI , status , Response , HTTPException
-from .. import schemas , db
-from .. import models
-from ..db import engine ,SessionLocal
-from sqlalchemy.orm import Session
+from typing import List
+from typing import Optional
+
+from fastapi import Request , Form
+import inspect
+from typing import Dict, Type
+
+from fastapi import Depends, FastAPI, File, Form
+from pydantic import BaseModel
+class Login:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.errors: List = []
+        self.username: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def load_data(self):
+        form = await self.request.form()
+        self.username = form.get(
+            "email"
+        )  # since outh works on username field we are considering email as username
+        self.password = form.get("password")
+
+    async def is_valid(self):
+        if not self.username or not (self.username.__contains__("@")):
+            self.errors.append("Email is required")
+        if not self.password or not len(self.password) >= 4:
+            self.errors.append("A valid password is required")
+        if not self.errors:
+            return True
+        return False
+
+def as_form(cls: Type[BaseModel]):
+    """
+    Adds an as_form class method to decorated models. The as_form class method
+    can be used with FastAPI endpoints
+    """
+    new_params = [
+        inspect.Parameter(
+            field.alias,
+            inspect.Parameter.POSITIONAL_ONLY,
+            default=(Form(field.default) if not field.required else Form(...)),
+            annotation=field.outer_type_,
+        )
+        for field in cls.__fields__.values()
+    ]
+
+    async def _as_form(**data):
+        return cls(**data)
+
+    sig = inspect.signature(_as_form)
+    sig = sig.replace(parameters=new_params)
+    _as_form.__signature__ = sig
+    setattr(cls, "as_form", _as_form)
+    return cls
+    
+@as_form
+class Item(BaseModel):
+    email: str
+    password: str
+    opts: Dict[str, int] = {}
--- crm-lead/routers/login.py
+++ crm-lead/routers/login.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends ,Request
-from fastapi import FastAPI , status , Response , HTTPException
+from fastapi import FastAPI , status , Response , HTTPException, Form
 from fastapi.responses import HTMLResponse
 from .. import schemas , models , db
 from ..db import engine ,SessionLocal,BASE_PATH
 from sqlalchemy.orm import Session
 from ..repos import login
+from ..repos .login import Login
 from fastapi.templating import Jinja2Templates
 import os
 from pathlib import Path
+from fastapi.encoders import jsonable_encoder
 
+get_db = db.get_db
 router = APIRouter(
     tags=['login'],
     prefix="/login",
@@ -21,3 +24,9 @@ async def login(request: Request):
     return TEMPLATES.TemplateResponse("login.html",{"request": request})
 
 
+@router.post("/")
+async def ps_login(request: Request):
+    form_data = await request.form()
+    da = jsonable_encoder(form_data)
+    print(form_data,"dfnkfn")
+    return da
